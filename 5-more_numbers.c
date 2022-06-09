ddb61e2cdca78f45c4238a95e018a710644bff85
faveKandie <obianujuofili03@gmail.com>
2022-06-09 01:53:57 +0100

    mistake

---
+++ 5-more_numbers.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - function that prints the numbers 0 to 9, followed by a new line
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		_putchar(n);
+	_putchar('\n');
+}
