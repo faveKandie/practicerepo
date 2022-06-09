305049e06d69f0b59fb0ce5ac00fedfb7b26aeb5
faveKandie <obianujuofili03@gmail.com>
2022-06-09 01:17:46 +0100

    trenches

---
+++ more_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdlib>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+	int n;
+
+	if (n = 'a'; n <= 'z'; n++)
+		return (0);
+	else
+		return (1);
+}
