699c45b3796b28092c52b564004bcf90538e3863
faveKandie <obianujuofili03@gmail.com>
2022-06-09 03:12:01 +0100

    good

---
+++ more_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always cee_am_oby
+ */
+int main(void)
+{
+	int n;
+
+	if (n > 0)
+	{
+		_putchar('_');
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
