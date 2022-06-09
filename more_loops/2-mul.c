03a2e4f2829512e5de2e743c1282247c8cd72aa9
faveKandie <obianujuofili03@gmail.com>
2022-06-09 01:45:25 +0100

    france

---
+++ more_loops/2-mul.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - function that multiplies two integers. Returns the value of the multiplication
+ * Return: result Success
+ */
+int main(void)
+{
+	int num1, num2, result;
+
+	for (num1 = 0; num1 >= 10; num++)
+	{
+		for (num2 = 0; num2 >= 10; num2++)
+		{
+			result = num1 * num2;
+
+			_putchar(result);
+		}
+		_putchar('\n');
+		
+		return (result);
+	}
+}
