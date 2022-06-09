8e64cd9e9e91ad704ea4bf9d6e1d12811507c861
faveKandie <obianujuofili03@gmail.com>
2022-06-09 03:19:56 +0100

    crap

---
+++ more_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+	int n;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\');
+		_putchar('\n');
+	}
+}
